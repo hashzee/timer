03608e6ff0638a9c746536815ae89c0997d82357
hashzee <55513359+hashzee@users.noreply.github.com>
2020-08-21 12:27:29 +0500

    Icons set

--- src/Components/Bottom/Bottom.tsx
+++ src/Components/Bottom/Bottom.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressCard, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle, faPlayCircle, faStopCircle, faRedo } from '@fortawesome/free-solid-svg-icons';
 
 const Bottom = (props:any) => {
     return (
         <nav className="nav">
             <a href="/#" className="nav__link startTimer" onClick={props.hStart}>
-            <i className="material-icons nav__icon"><FontAwesomeIcon icon={faUser} /></i>Start
+            <i className="material-icons nav__icon"><FontAwesomeIcon icon={faPlayCircle} /></i>Start
             </a>
             <a href="/#" className="nav__link stopTimer" onClick={props.hStop}>
-            <i className="material-icons nav__icon"><FontAwesomeIcon icon={faUserCircle} /></i>Stop
+            <i className="material-icons nav__icon"><FontAwesomeIcon icon={faStopCircle} /></i>Stop
             </a>
             <a href="/#" className="nav__link resetTimer" onClick={props.hReset}>
-            <i className="material-icons nav__icon" ><FontAwesomeIcon icon={faAddressCard} /></i>Reset
+            <i className="material-icons nav__icon" ><FontAwesomeIcon icon={faRedo} /></i>Reset
             </a>
             <a href="/#" className="nav__link handleAbout" onClick={props.hAbout}>
-            <i className="material-icons nav__icon"><FontAwesomeIcon icon={faAddressCard} /></i>About
+            <i className="material-icons nav__icon"><FontAwesomeIcon icon={faInfoCircle} /></i>About
             </a>
         </nav> 
     )
